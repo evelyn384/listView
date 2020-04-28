6edb68a51e7f40f9a84e0954ce51893fd78af222
evely <evelyn030298@outlook.com>
2020-04-27 19:06:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val animales = listOf("Gato", "Perro", "Leon", "Pinguino", "Minotauro","Padino mágico")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, animales )
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this,"${animales[position]}",Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
